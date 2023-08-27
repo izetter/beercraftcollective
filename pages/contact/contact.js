b8827244031579b14c8c04162e2a375de53b908c
izetter <59151193+izetter@users.noreply.github.com>
2023-08-26 23:30:47 -0600

    Validation for all inputs

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -3,7 +3,7 @@ const nameInput = form.elements['name'];
 const emailInput = form.elements['email'];
 const textArea = form.elements['message'];
 const formInputs = [nameInput, emailInput, textArea];
-const validChars = new RegExp(/[A-Zñáéíóúüçäëïöàèìòù\s]/i);
+const validNameChars = new RegExp(/[A-Zñáéíóúüçäëïöàèìòù\s]/i);
 let hasClickedSubmit = false;
 
 const endpoint = 'https://formsubmit.co/5f0f9995143bfb7bcf4d49ef2a9749b1';
@@ -22,49 +22,23 @@ async function postMessage(body, url = endpoint) {
 	}
 }
 
-function validateInput(input) {
-	if (input.value) {
-		input.classList.remove('border-dark-subtle');
-		input.classList.add('is-valid');
-		return true;
-	} else {
-		input.classList.remove('border-dark-subtle');
-		input.classList.add('is-invalid');
-		return false;
-	}
-}
-
-function isFormValid() {
-	// return formInputs.some((input) => validateInput(input));
-	return false;
-}
-
-function handleNameInput() {
-	if (hasClickedSubmit) {
-	} else {
-	}
-}
-
-function validateChar(evt) {
-	if (!validChars.test(evt.data)) evt.preventDefault();
+function validateNameChar(evt) {
+	if (!validNameChars.test(evt.data)) evt.preventDefault();
 }
 
 function setValidInput(input) {
 	input.classList.remove('border-dark-subtle', 'is-invalid');
 	input.classList.add('is-valid');
-	input.dataset.validated = true;
 }
 
 function setInvalidInput(input) {
 	input.classList.remove('border-dark-subtle', 'is-valid');
 	input.classList.add('is-invalid');
-	input.dataset.validated = true;
 }
 
 function setDefaultInput(input) {
 	input.classList.remove('is-valid', 'is-invalid');
 	input.classList.add('border-dark-subtle');
-	input.dataset.validated = false;
 }
 
 function onInput(evt) {
@@ -74,8 +48,7 @@ function onInput(evt) {
 	} else if (hasClickedSubmit && target.value === '') {
 		setInvalidInput(target);
 	} else if (target.value === '') {
-		setInvalidInput(target);
-		// setDefaultInput(target);
+		setDefaultInput(target);
 	}
 }
 
@@ -85,22 +58,40 @@ function onBlur(evt) {
 	}
 }
 
+function validateInput(input) {
+	if (input.value) {
+		setValidInput(input);
+		return true;
+	} else {
+		setInvalidInput(input);
+		return false;
+	}
+}
+
+function isFormValid() {
+	return formInputs.map((input) => validateInput(input)).every((validity) => validity === true);
+}
+
 function handleSubmit(evt) {
 	evt.preventDefault();
 	hasClickedSubmit = true;
 	if (isFormValid()) {
 		// postMessage(form);
+		hasClickedSubmit = false;
 		console.log('Valid');
 		formInputs.forEach((input) => setDefaultInput(input));
 	}
 	console.log('Not valid');
 }
 
-function checkEmptyInputs() {}
-
 form.addEventListener('submit', (evt) => handleSubmit(evt));
-nameInput.addEventListener('beforeinput', (evt) => validateChar(evt));
-nameInput.addEventListener('input', (evt) => onInput(evt));
-nameInput.addEventListener('blur', (evt) => onBlur(evt));
+nameInput.addEventListener('beforeinput', (evt) => validateNameChar(evt));
+formInputs.forEach((input) => {
+	input.addEventListener('input', (evt) => onInput(evt));
+	input.addEventListener('blur', (evt) => onBlur(evt));
+})
+
+// nameInput.addEventListener('input', (evt) => onInput(evt));
+// nameInput.addEventListener('blur', (evt) => onBlur(evt));
 // emailInput.addEventListener('blur', (evt) => validateNameInput(evt.target));
 // textArea.addEventListener('blur', (evt) => validateNameInput(evt.target));
