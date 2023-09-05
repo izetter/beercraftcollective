2745315f1ae95595c2ee9416a39dc16ae250b347
izetter <59151193+izetter@users.noreply.github.com>
2023-09-05 14:24:29 -0600

    Setting up index page

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello world');
+import { navbar } from './components/navbar.js';
+import { footer } from './components/footer.js';
+import { productCard } from './components/productCard.js';
+import { sampleProductList } from './assets/sampleProductList.js'; // This object is just to simulate the values ENTERED in the form, NOT to simulate the RESULTS of many form submissions that get processed by addBeer...
+import { BeerController } from './utils/BeerController.js';
+
+const myBeers = new BeerController();
+sampleProductList.forEach((beer) => myBeers.addBeer(beer));
+
+const productSection = document.getElementById('product-section');
+myBeers.items.forEach((beer) => {
+	const cardTemplate = document.createElement('template');
+	cardTemplate.innerHTML = productCard(beer);
+	productSection.append(cardTemplate.content);
+});
+
+const navTemplate = document.createElement('template');
+navTemplate.innerHTML = navbar();
+document.querySelector('nav').replaceWith(navTemplate.content);
+
+const footerTemplate = document.createElement('template');
+footerTemplate.innerHTML = footer();
+document.querySelector('footer').replaceWith(footerTemplate.content);
