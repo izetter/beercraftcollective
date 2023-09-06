69149eea82d23240c42087e9716501e1021256b3
izetter <59151193+izetter@users.noreply.github.com>
2023-09-05 23:08:16 -0600

    typos

--- components/footer.js
+++ components/footer.js
@@ -24,10 +24,10 @@ export function footer() {
 								<a class="nav-link text-black py-0" href="/pages/cart/cart.html">Mi carrito</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link text-black py-0" href="/pages/users/users.html">Acceso a clientes</a>
+								<a class="nav-link text-black py-0" href="/pages/users/users.html">Login</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link text-black py-0" href="/pages/access/access.html">Administrador</a>
+								<a class="nav-link text-black py-0" href="/pages/access/access.html">Admin</a>
 							</li>
 						</ul>
 					</div>
