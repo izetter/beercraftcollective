05a0e55bc200526e25739f8881eab7bda799d78a
izetter <59151193+izetter@users.noreply.github.com>
2023-09-08 16:23:43 -0600

    small refactor

--- pages/new-user/newUsers.js
+++ pages/new-user/newUsers.js
@@ -1,10 +1,6 @@
 import { footer } from '../../components/footer.js';
 import { navbar } from '../../components/navbar.js';
 
-const footerElement = document.querySelector('footer');
-
-footerElement.innerHTML = footer();
-
 const navTemplate = document.createElement('template');
 navTemplate.innerHTML = navbar();
 document.querySelector('nav').replaceWith(navTemplate.content);
@@ -13,12 +9,14 @@ const footerTemplate = document.createElement('template');
 footerTemplate.innerHTML = footer();
 document.querySelector('footer').replaceWith(footerTemplate.content);
 
-document.getElementById('registroForm').addEventListener('submit', function (event) {
+const users = [];
+
+document.getElementById('registro-form').addEventListener('submit', function (event) {
 	event.preventDefault(); // Prevenir la recarga de la página
 
 	// Obtener los valores del formulario
-	const nombreCompleto = document.getElementById('nombreCompleto').value;
-	const telefono = document.getElementById('telefono').value;
+	const name = document.getElementById('nombreCompleto').value;
+	const phone = document.getElementById('telefono').value;
 	const email = document.getElementById('email').value;
 	const password = document.getElementById('password').value;
 	const confirmPassword = document.getElementById('confirmPassword').value;
@@ -38,21 +36,22 @@ document.getElementById('registroForm').addEventListener('submit', function (eve
 
 	// Validar el formato del número de teléfono
 	const phoneRegex = /^\d{10}$/; // Suponiendo que el número tiene 10 dígitos
-	if (!phoneRegex.test(telefono)) {
+	if (!phoneRegex.test(phone)) {
 		alert('El número de teléfono no es válido.');
 		return;
 	}
 
 	// Crear un objeto JSON con los campos del usuario
-	const usuario = {
-		nombreCompleto: nombreCompleto,
-		telefono: telefono,
-		email: email,
-		password: password,
-	};
+	users.push({
+		name,
+		phone,
+		email,
+		password,
+	});
 
 	// Aquí puedes realizar otras acciones, como enviar los datos a un servidor
 
 	// Mostrar un mensaje de éxito
-	alert('Registro exitoso:\n' + JSON.stringify(usuario, null, 2));
+	console.log(users);
+	alert('Registro exitoso');
 });
