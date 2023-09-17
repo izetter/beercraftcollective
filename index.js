0e6c8e9b958d9a209ad76ae0cfdccdf14bfa57ed
izetter <59151193+izetter@users.noreply.github.com>
2023-09-16 22:24:57 -0600

    Main/index page reflects the state of the products in local storage

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import { navbar } from './components/navbar.js';
 import { footer } from './components/footer.js';
 import { productCard } from './components/productCard.js';
-import { sampleProductList } from './assets/sampleProductList.js'; // This object is just to simulate the values ENTERED in the form, NOT to simulate the RESULTS of many form submissions that get processed by addBeer...
-import { BeerController } from './utils/BeerController.js';
-
-const myBeers = new BeerController();
-sampleProductList.forEach((beer) => myBeers.addBeer(beer));
 
 const productSection = document.getElementById('product-section');
-myBeers.items.forEach((beer) => {
+const products = JSON.parse(localStorage.getItem('products'));
+products.forEach((product) => {
 	const cardTemplate = document.createElement('template');
-	cardTemplate.innerHTML = productCard(beer);
+	cardTemplate.innerHTML = productCard(product);
 	productSection.append(cardTemplate.content);
 });
 
