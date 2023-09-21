300fa815a007bf02c82ba534f06b7edfaa555a9b
Esmeralda <esmeraldatorres.2305@gmail.com>
2023-09-21 01:06:39 -0600

    navBar

--- components/navbar.js
+++ components/navbar.js
@@ -1,28 +1,53 @@
+import cart from '../assets/img/cart.png';
+import user from '../assets/img/user.png';
+
 import logo from '../assets/img/logo.png';
 
 export function navbar() {
 	return `
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="/index.html">
-					<img src="${logo}" alt="Logo" width="50" height="50" class="rounded-circle align-text-">
-					Beer Craft Collective</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse " id="navbarSupportedContent">
-					<div class="navbar-nav mb-1 mx-3">
-						<a href="/index.html" aria-current="page"
-							class="nav-item nav-link mx-5">Inicio</a>
-						<a href="/pages/about/about.html" class="nav-item nav-link mx-5">Nosotros</a>
-						<a href="/pages/contact/contact.html" class="nav-item nav-link mx-5">Contacto</a>
-						<a href="/pages/cart/cart.html" class="nav-item nav-link mx-5">Carrito</a>
-						<a href="/pages/user-login/userLogin.html" class="nav-item nav-link mx-5">Login</a>
-						</div>
-				</div>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/index.html">
+			<img src="${logo}" alt="Logo" width="50" height="50" class="rounded-circle align-text-" />
+			Beer Craft Collective</a
+		>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+			<div class="navbar-nav mb-1 mx-1">
+				<a href="/index.html" aria-current="page" class="nav-item nav-link mx-5">Inicio</a>
+				<a href="/pages/about/about.html" class="nav-item nav-link mx-5">Nosotros</a>
+				<a href="/pages/contact/contact.html" class="nav-item nav-link mx-5">Contacto</a>
 			</div>
-		</nav>
+			<div class="navbar-nav mb-1 mx-1">
+				<a href="/pages/cart/cart.html" class="nav-item nav-link mx-3">
+					<img
+						src="${cart}"
+						alt="cart"
+						width="30"
+						height="30"
+						class="align-text-center"
+					/>Carrito</a
+				>
+
+				<a href="/pages/user-login/userLogin.html" class="nav-item nav-link mx-3">
+					<img src="${user}" alt="" width="30" height="30" class="align-text-center" />
+					Login</a
+				>
+			</div>
+		</div>
+	</div>
+</nav>
+
+
 	`;
 }
