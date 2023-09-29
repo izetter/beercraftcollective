20e63e52329d2618c60af3cf45cea8fff8f9228a
diegoanh <diego.a.herrera.garcia@gmail.com>
2023-09-28 23:44:41 -0600

    login user admin

--- pages/admin-login/adminLogin.js
+++ pages/admin-login/adminLogin.js
@@ -12,3 +12,23 @@ document.querySelector('nav').replaceWith(navTemplate.content);
 const footerTemplate = document.createElement('template');
 footerTemplate.innerHTML = footer();
 document.querySelector('footer').replaceWith(footerTemplate.content);
+
+const usuariosAutorizados = [{ correo: 'admin@beercraft.org', contraseña: 'contraseña123' }];
+
+document.querySelector('form').addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	const emailInput = document.getElementById('exampleInputEmail1');
+	const passwordInput = document.getElementById('exampleInputPassword1');
+
+	const email = emailInput.value;
+	const password = passwordInput.value;
+
+	const usuarioValido = usuariosAutorizados.find((user) => user.correo === email && user.contraseña === password);
+
+	if (usuarioValido) {
+		window.location.href = '../admin/admin.html';
+	} else {
+		alert('Credenciales inválidas. Inténtalo de nuevo.');
+	}
+});
